9f70da2a1977a4ec1bef7df17bfff54589ac1d13
Rachel Peng <rachelvpeng@gmail.com>
2021-02-02 19:06:57 -0500

    implemented school, added random school types

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,6 @@
+import random
+
 def draw_school():
-    print("[This is a school]")
+    school_type = ['high', 'middle', 'elementary']
+    print("[This is a " + random.choice(school_type) + " school]")
     return
